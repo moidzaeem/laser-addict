649e0db2b6077db811e68a6799fb003a50961b2f
noumandatu <noumankhan619.915@gmail.com>
2024-06-07 17:13:24 +0500

    asd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,6 +10,7 @@ import {
 import { Font, Heading } from "../utils/theme/typo";
 import AppButton from "./Button";
 import { primary } from "../utils/theme/colors";
+import { Div } from "../utils/styled-components";
 
 const Footer = () => {
   return (
@@ -20,22 +21,29 @@ const Footer = () => {
             <img src="/images/logo.png" alt="Logo" style={{ width: "150px" }} />
           </Font>
           <Font variant="body1">
-            LASERADDICT® WELLNESS CENTER - LASER SMOKING CESSATION - SUPPORT -
+            LASERADDICT® WELLNESS CENTER - LASER SMOKING CESSATION - SUPPORT -{" "}
+            <br />
             COACHING
           </Font>
-          <Box mt={2}>
+          <Box mt={12}>
             <Heading variant="body1" gutterBottom>
               Reach out to
             </Heading>
-            <Box>
+            <Box sx={{ display: "flex", mt: 3 }}>
               <Link href="#" color="inherit" sx={{ mr: 2 }}>
-                <Facebook fontSize="large" />
+                <Iconbtn color="black">
+                  <Facebook />
+                </Iconbtn>
               </Link>
               <Link href="#" color="inherit" sx={{ mr: 2 }}>
-                <Instagram fontSize="large" />
+                <Iconbtn color="black">
+                  <Instagram />
+                </Iconbtn>
               </Link>
               <Link href="#" color="inherit">
-                <LinkedIn fontSize="large" />
+                <Iconbtn color="black">
+                  <LinkedIn />
+                </Iconbtn>
               </Link>
             </Box>
           </Box>
@@ -52,7 +60,7 @@ const Footer = () => {
                 </Typography>
                 <Font>Health & Wellness</Font>
               </Grid>
-              <Grid item xs={12}>
+              <Grid sx={{ mt: 10 }} item xs={12}>
                 <Heading variant="h6" gutterBottom>
                   Information
                 </Heading>
@@ -72,7 +80,7 @@ const Footer = () => {
                 <Font>Our Headquarters</Font>
                 <Font>News</Font>
               </Grid>
-              <Grid item xs={12}>
+              <Grid sx={{ mt: 7 }} item xs={12}>
                 <Heading variant="h6" gutterBottom>
                   Legal Information
                 </Heading>
@@ -86,21 +94,27 @@ const Footer = () => {
           <Heading variant="h6" gutterBottom>
             Contact Information
           </Heading>
-          <Box display="flex" alignItems="center" mb={1}>
-            <LocationOn sx={{ mr: 1 }} />
+          <Box mt={4} display="flex" alignItems="center" mb={1}>
+            <Iconbtn>
+              <LocationOn />
+            </Iconbtn>
             <Font>1 Rue des Pins, 38100 Grenoble, France</Font>
           </Box>
           <Box display="flex" alignItems="center" mb={1}>
-            <Phone sx={{ mr: 1 }} />
+            <Iconbtn>
+              <Phone />
+            </Iconbtn>
             <Font variant="body1">06 49 56 70 82</Font>
           </Box>
           <Box display="flex" alignItems="center" mb={1}>
-            <Email sx={{ mr: 1 }} />
+            <Iconbtn>
+              <Email />
+            </Iconbtn>
             <Font variant="body1">contact@laseraddict.fr</Font>
           </Box>
-          <Box mt={2}>
+          <Box mt={7} mb={3}>
             <Heading>Our Newsletter</Heading>
-            <Font>
+            <Font mt={3}>
               Stay connected and be the first to know. Subscribe to our
               newsletter for exclusive updates.
             </Font>
@@ -127,3 +141,29 @@ const Footer = () => {
 };
 
 export default Footer;
+
+interface IconBtnType {
+  children: React.ReactNode;
+  color?: string;
+}
+
+const Iconbtn = ({ children, color = primary }: IconBtnType) => {
+  return (
+    <Div
+      sx={{
+        background: color,
+        borderRadius: "100%",
+        color: "white",
+        width: 40,
+        height: 40,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        mr: 1,
+        mb: 1,
+      }}
+    >
+      {children}
+    </Div>
+  );
+};
