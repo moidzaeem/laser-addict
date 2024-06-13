8182c9af40d29b61f133ef90fa45ea5baaa8bfa6
noumandatu <noumankhan619.915@gmail.com>
2024-06-13 20:17:30 +0500

    asd

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -2,6 +2,8 @@ import { CardContent } from "@mui/material";
 import { Font, Label } from "../utils/theme/typo";
 import { Div } from "../utils/styled-components";
 import { primary } from "../utils/theme/colors";
+import StarIcon from "@mui/icons-material/Star";
+import Radio from "@mui/material/Radio";
 
 interface ServiceCardProps {
   service: any;
@@ -19,26 +21,61 @@ const ServiceCard = ({
   return (
     <Div
       onClick={() => setSelectedService(service.name)}
-      className="shadow"
       sx={{
         minWidth: 180,
         m: 2,
         borderRadius: 4,
         cursor: "pointer",
-        border: "1px solid",
-        borderColor: isSelected ? primary : "white",
+        border: "2px solid",
+        borderColor: isSelected ? "#7ED1A1" : "#D9D9D9",
         transition: "border-color 0.3s ease-in-out",
         "&:hover": {
           borderColor: primary,
         },
+        boxShadow: "0px 6.71px 18.31px 12.21px rgba(0, 0, 0, 0.05);",
       }}
     >
       <CardContent>
-        <img style={{ height: 50 }} src={service.img} alt={service.name} />
-        <Font>{service.name}</Font>
+        <Div
+          width={"100%"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <img style={{ height: 40 }} src={service.img} alt={service.name} />
+          <img
+            style={{ height: 20 }}
+            src={"/images/11.png"}
+            alt={service.name}
+          />
+        </Div>
+        <Font sx={{ fontWeight: "bold", mt: 1 }}>{service.name}</Font>
         {service.sessions && <Label sx={{ mt: 1 }}>{service.sessions}</Label>}
         <Label>{service.duration}</Label>
-        <Font sx={{ mt: 1 }}>{service.price}</Font>
+        {[1, 2, 3, 4, 5].map(() => (
+          <StarIcon sx={{ width: 15, color: primary, mt: 1 }} />
+        ))}
+        <Div
+          width={"100%"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Font sx={{ fontWeight: "bold", fontSize: 20, mt: 1 }}>
+            {service.price}
+          </Font>
+          <Radio
+            checked
+            sx={{
+              color: "#D9D9D9",
+              "&.Mui-checked": {
+                color: "#D9D9D9",
+              },
+              position: "relative",
+              left: 10,
+            }}
+          />
+        </Div>
       </CardContent>
     </Div>
   );
